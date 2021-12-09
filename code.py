36f00394553614347c0f39324c5153d3d87529f2
rebeccakhachaya <rebeccakhachaya@gmail.com>
2021-12-09 20:26:51 +0300

    commit

--- code.py
+++ code.py
@@ -33,14 +33,14 @@ dif(100)
 
 # Number 3
 
-def collatz(number):
-    while number != 1:
-        if number % 2 == 0:
-            number = number // 2
-            print(number)
+def collatz(n):
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+            print(n)
             
         else:
-            number = (3 * number + 1)
-            print(number)
+            n = (3 * n + 1)
+            print(n)
 
 collatz(12)
